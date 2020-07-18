5a30f218e7fe105a463e142179efddc9363703b6
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-07-18 14:26:32 +0400

    api

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 router.use((req, res, next) => {
-    
+    console.log('A')
     console.log(req.get('Origin'))
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
