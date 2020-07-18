f81efc101ddc4efe16806b1cf2b64959a8dec27d
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-07-18 14:56:35 +0400

    mobile

--- docs/js/func.js
+++ docs/js/func.js
@@ -1,13 +1,11 @@
+console.log(Theme)
 var funcObj = {
     clearScreen() {
         messages.innerHTML = ''
         messages.scrollTop = messages.scrollHeight;
     },
     changeTheme() {
-        var Theme = {
-            dark:'./css/dark.css',
-            light:'./css/default.css'
-        }
+        
         var themeCSS = document.getElementById('theme-stylesheet')
         if (themeCSS.getAttribute('href') === Theme.dark){
             themeCSS.setAttribute('href',Theme.light)
