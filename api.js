4c27c6e0252c39b99a0c7e0d81b2580a8e3acf62
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-07-18 14:29:28 +0400

    api

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ router.use(bodyParser.json())
 router.use((req, res, next) => {
     console.log('A')
     console.log(req.get('Origin'))
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://yunisdev.github.io http://locahost:3000");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
